6d04505ee78bcd7e5b94d127f3049c45494ee799
Mohan Chaudhary <62174702+mohan05542@users.noreply.github.com>
2022-10-27 16:19:46 +0530

    Update bollywood.js

--- js/bollywood.js
+++ js/bollywood.js
@@ -14,7 +14,7 @@ let newsPro = document.getElementById('content');
 
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://api.jsonbin.it/bins/NirhUyS1',true);
+xhr.open('GET','https://api.npoint.io/0de7870b4aba590dc349',true);
 // xhr.open('GET','https://api.publicapis.org/entries',true);
 xhr.getResponseHeader('Content-type','application/json');
 
