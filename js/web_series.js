8a9671319d9a3be88759f7352ea4c5ea8cddb4fa
Mohan Chaudhary <62174702+mohan05542@users.noreply.github.com>
2022-10-27 16:35:25 +0530

    Update web_series.js

--- js/web_series.js
+++ js/web_series.js
@@ -14,7 +14,7 @@ let newsPro = document.getElementById('content');
 
 
 var xhr = new XMLHttpRequest();
-xhr.open('GET','https://api.npoint.io/7cc6e2ea80e7f65f2',true);
+xhr.open('GET','https://api.npoint.io/7cc6e2ea80e7f65f2ec0',true);
 // xhr.open('GET','https://api.publicapis.org/entries',true);
 xhr.getResponseHeader('Content-type','application/json');
 
